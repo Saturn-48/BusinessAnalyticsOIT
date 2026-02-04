873f851a27bfa424ac4cca41ab2d382c5e2d13ca
aiden-laptop <aidenracer48@gmail.com>
2026-02-03 16:26:08 -0800

    Made DB and formatted it

--- Database_Creation.py
+++ Database_Creation.py
@@ -12,21 +12,21 @@ def Setup():
     def ConvertCSV():
         conn, cursor = NewConn()
 
-        query = ("CREATE TABLE IF NOT EXISTS Sales("
+        query = ("CREATE TABLE IF NOT EXISTS Sales AS "
                  "SELECT "
-                 "Date, "
-                 "Weekly_Sales, "
-                 "Holiday_Flag, "
-                 "Temperature, "
-                 "Fuel_Price, "
-                 "Unemployment "
-                 "FROM read_csv() "
+                 "Date AS Date, "
+                 "Weekly_Sales AS Weekly_Sales, "
+                 "Holiday_Flag AS Holiday_Flag, "
+                 "Temperature AS Temperature, "
+                 "Fuel_Price AS Fuel_Price, "
+                 "Unemployment AS Unemployment "
+                 "FROM read_csv('Walmart_Store_Sales.csv', HEADER=True) "
                  "WHERE Date IS NOT NULL AND "
                  "Weekly_Sales IS NOT NULL AND "
                  "Holiday_Flag IS NOT NULL AND "
                  "Temperature IS NOT NULL AND "
                  "Fuel_Price IS NOT NULL AND "
-                 "Unemployment IS NOT NULL)")
+                 "Unemployment IS NOT NULL")
 
         cursor.execute(query)
         conn.commit()
@@ -34,15 +34,21 @@ def Setup():
 
     def FormatData():
         conn, cursor = NewConn()
-        query = ("UPDATE Sales "
-                 "SET Weekly_Sales = ROUND(Weekly_Sales, 2)"
-                 "UPDATE Sales "
-                 "SET Temperature = ROUND(Temperature, 2)"
-                 "UPDATE Sales "
-                 "SET Fuel_Price = ROUND(Fuel_Price, 3)"
-                 "UPDATE Sales "
-                 "SET Unemployment = ROUND(Unemployment, 1)")
-        cursor.executemany(query)
+        q1 = ("UPDATE Sales "
+              "SET Weekly_Sales = ROUND(Weekly_Sales, 2) ")
+        q2 = ("UPDATE Sales "
+             "SET Temperature = ROUND(Temperature, 2)")
+
+        q3 = ("UPDATE Sales "
+             "SET Fuel_Price = ROUND(Fuel_Price, 3)")
+
+        q4 = ("UPDATE Sales "
+             "SET Unemployment = ROUND(Unemployment, 1)")
+        cursor.execute(q1)
+        cursor.execute(q2)
+        cursor.execute(q3)
+        cursor.execute(q4)
+
         conn.commit()
         conn.close()
 
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from Database_Creation import Setup
 
 
 Setup()
+
